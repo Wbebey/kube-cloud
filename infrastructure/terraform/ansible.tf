a09eb4aa00990175d8ee45a89487bcaba3740a57
Wbebey <wilfried@armis.tech>
2023-07-13 10:52:44 +0200

    feat: add nginx controller

--- infrastructure/terraform/ansible.tf
+++ infrastructure/terraform/ansible.tf
@@ -1,35 +1,35 @@
-locals {
-  ssh_private_key_path  = "~/.ssh/group-3_rsa"
-  ansible_user          = "admin-terraform-kubi-zdt"
-  ansible_host_path     = "ansible/config/hosts.ini"
-  ansible_playbook_path = "ansible/playbooks/setup-k8s.yaml"
-}
-
-resource "local_file" "ansible_hosts" {
-  content = <<-EOF
-    [master]
-    master10 ansible_host=${google_compute_instance.vm-kube[0].network_interface.0.access_config.0.nat_ip}
-
-    [workers]
-    worker20 ansible_host=${google_compute_instance.vm-kube[1].network_interface.0.access_config.0.nat_ip}
-    worker30 ansible_host=${google_compute_instance.vm-kube[2].network_interface.0.access_config.0.nat_ip}
-
-    [all:vars]
-    ansible_python_interpreter=/usr/bin/python3
-    ansible_user=${local.ansible_user}
-    ansible_ssh_private_key_file=${local.ssh_private_key_path}
-  EOF
-
-  filename   = local.ansible_host_path
-  depends_on = [google_compute_address.public-ip-vm-kube]
-}
-
-# resource "null_resource" "ansible_provisioner" {
-#   depends_on = [
-#     local_file.ansible_hosts
-#   ]
-
-#   provisioner "local-exec" {
-#     command = "ansible-playbook -i ${local.ansible_host_path} ${local.ansible_playbook_path}"
-#   }
+# locals {
+#   ssh_private_key_path  = "~/.ssh/group-3_rsa"
+#   ansible_user          = "admin-terraform-kubi-zdt"
+#   ansible_host_path     = "ansible/config/hosts.ini"
+#   ansible_playbook_path = "ansible/playbooks/setup-k8s.yaml"
 # }
+
+# resource "local_file" "ansible_hosts" {
+#   content = <<-EOF
+#     [master]
+#     master10 ansible_host=${google_compute_instance.vm-kube[0].network_interface.0.access_config.0.nat_ip}
+
+#     [workers]
+#     worker20 ansible_host=${google_compute_instance.vm-kube[1].network_interface.0.access_config.0.nat_ip}
+#     worker30 ansible_host=${google_compute_instance.vm-kube[2].network_interface.0.access_config.0.nat_ip}
+
+#     [all:vars]
+#     ansible_python_interpreter=/usr/bin/python3
+#     ansible_user=${local.ansible_user}
+#     ansible_ssh_private_key_file=${local.ssh_private_key_path}
+#   EOF
+
+#   filename   = local.ansible_host_path
+#   depends_on = [google_compute_address.public-ip-vm-kube]
+# }
+
+# # resource "null_resource" "ansible_provisioner" {
+# #   depends_on = [
+# #     local_file.ansible_hosts
+# #   ]
+
+# #   provisioner "local-exec" {
+# #     command = "ansible-playbook -i ${local.ansible_host_path} ${local.ansible_playbook_path}"
+# #   }
+# # }
--- infrastructure/terraform/compute.tf
+++ infrastructure/terraform/compute.tf
@@ -1,33 +1,33 @@
-data "google_client_openid_userinfo" "admin-user-info" {}
+# data "google_client_openid_userinfo" "admin-user-info" {}
 
-resource "google_compute_instance" "vm-kube" {
-  count          = length(var.vm_names)
-  name           = var.vm_names[count.index]
-  machine_type   = var.machine_types[count.index]
-  zone           = "${var.gcp_region}-b"
-  project        = google_project.kubi-cloud.project_id
-  tags           = ["ssh", "app", "kube"]
-  can_ip_forward = true
+# resource "google_compute_instance" "vm-kube" {
+#   count          = length(var.vm_names)
+#   name           = var.vm_names[count.index]
+#   machine_type   = var.machine_types[count.index]
+#   zone           = "${var.gcp_region}-b"
+#   project        = google_project.kubi-cloud.project_id
+#   tags           = ["ssh", "app", "kube"]
+#   can_ip_forward = true
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-  metadata = {
-    ssh-keys               = <<-EOT
-      ${split("@", data.google_client_openid_userinfo.admin-user-info.email)[0]}:${var.ssh_mathieu}
-      ${split("@", data.google_client_openid_userinfo.admin-user-info.email)[0]}:${var.ssh_willy}
-    EOT
-    startup-script         = "sudo apt-get update && sudo apt-get install -y curl && curl -sSL https://get.docker.com/ | sh && sudo usermod -aG docker admin-terraform-kubi-zdt && sudo systemctl restart docker"
-    block-project-ssh-keys = "true"
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
+#   metadata = {
+#     ssh-keys               = <<-EOT
+#       ${split("@", data.google_client_openid_userinfo.admin-user-info.email)[0]}:${var.ssh_mathieu}
+#       ${split("@", data.google_client_openid_userinfo.admin-user-info.email)[0]}:${var.ssh_willy}
+#     EOT
+#     startup-script         = "sudo apt-get update && sudo apt-get install -y curl && curl -sSL https://get.docker.com/ | sh && sudo usermod -aG docker admin-terraform-kubi-zdt && sudo systemctl restart docker"
+#     block-project-ssh-keys = "true"
+#   }
 
-  network_interface {
-    network    = google_compute_network.kube_vpc_network.name
-    subnetwork = google_compute_subnetwork.kube_vpc_subnetwork.self_link
-    access_config {
-      nat_ip = google_compute_address.public-ip-vm-kube[count.index].address
-    }
-  }
-}
+#   network_interface {
+#     network    = google_compute_network.kube_vpc_network.name
+#     subnetwork = google_compute_subnetwork.kube_vpc_subnetwork.self_link
+#     access_config {
+#       nat_ip = google_compute_address.public-ip-vm-kube[count.index].address
+#     }
+#   }
+# }
